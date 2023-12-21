ae8f022662ffcf50d7821af739137ebb5b2aa85d
isumalatov <isumalatov@gmail.com>
2023-12-21 20:47:44 +0100

    fixing error

--- ilyas_umalatov.py
+++ ilyas_umalatov.py
@@ -43,14 +43,12 @@ class DecisionStump:
         return predictions
 
 class Adaboost:
-    def __init__(self, T=40, A=20):
+    def __init__(self, T, A):
         self.T = T
         self.A = A
         self.clfs = []
-        self.train_time = None
 
-    def fit(self, X, Y, verbose=False):
-        start_time = time.time()
+    def fit(self, X, Y):
         n_samples, n_features = X.shape
         W = np.full(n_samples, (1 / n_samples))
         for _ in range(self.T):
@@ -65,8 +63,6 @@ class Adaboost:
             W *= np.exp(-alpha * Y * best_predictions)
             W /= np.sum(W)
             self.clfs.append((best_clf, alpha))
-        end_time = time.time()
-        self.train_time = end_time - start_time
 
     def predict(self, X):
         clf_preds = [alpha * clf.predict(X) for clf, alpha in self.clfs]
@@ -81,64 +77,59 @@ def adaboost_test(class_label, T, A):
 
     # Convert labels to -1 and 1
     Y_train = np.where(Y_train == class_label, 1, -1)
-    Y_test = np.where(Y_test == class_label, 1, -1)
 
     # Create and train Adaboost classifier
     clf = Adaboost(T, A)
 
-    # Make predictions
+    start_time = time.time()
+    clf.fit(X_train, Y_train)
     y_train_pred = clf.predict(X_train)
-    y_test_pred = clf.predict(X_test)
+    end_time = time.time()
 
     # Calculate accuracy scores
     train_accuracy = accuracy_score(Y_train, y_train_pred)
-    test_accuracy = accuracy_score(Y_test, y_test_pred)
+
 
     # Print accuracy scores
     print(f'Train accuracy: {train_accuracy * 100}%')
-    print(f'Test accuracy: {test_accuracy * 100}%')
-    print(f'Time: {clf.train_time} s')
+    print(f'Time: {end_time - start_time} s')
 
     # Print additional information
     for i, (clf, alpha) in enumerate(clf.clfs, 1):
         print(f'Añadido clasificador {i}: {clf.feature_index}, {clf.threshold}, {clf.polarity}, {alpha}')
 
 #Tarea 1C
-def adaboost_grafic():
+def adaboost_grafic(class_label):
     # Define the values of T and A to test
     T_values = [10, 20, 30, 40, 50]
     A_values = [10, 20, 30, 40, 50]
 
     # Initialize empty lists for storing results
     train_accuracies = []
-    test_accuracies = []
     train_times = []
 
     # Load MNIST data
     X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
 
     # Convert labels to -1 and 1
-    Y_train = np.where(Y_train == 0, 1, -1)
-    Y_test = np.where(Y_test == 0, 1, -1)
+    Y_train = np.where(Y_train == class_label, 1, -1)
 
     for T in T_values:
         for A in A_values:
             # Create and train Adaboost classifier
             clf = Adaboost(T, A)
+            start_time = time.time()
             clf.fit(X_train, Y_train)
 
             # Make predictions
             y_train_pred = clf.predict(X_train)
-            y_test_pred = clf.predict(X_test)
-
+            end_time = time.time()
             # Calculate accuracy scores
             train_accuracy = accuracy_score(Y_train, y_train_pred)
-            test_accuracy = accuracy_score(Y_test, y_test_pred)
 
             # Store results
             train_accuracies.append(train_accuracy)
-            test_accuracies.append(test_accuracy)
-            train_times.append(clf.train_time)
+            train_times.append(end_time - start_time)
 
     # Reshape the results
     train_accuracies = np.array(train_accuracies).reshape(len(T_values), len(A_values))
@@ -170,12 +161,12 @@ def adaboost_grafic():
 
 #Tarea 1D
 class Adaboost2:
-    def __init__(self, T=40, A=20):
+    def __init__(self, T, A):
         self.T = T
         self.A = A
         self.clfs = []
 
-    def fit(self, X, Y, verbose=False):
+    def fit(self, X, Y):
         n_samples, n_features = X.shape
         W = np.full(n_samples, (1 / n_samples))
         for _ in range(self.T):
@@ -198,12 +189,11 @@ class Adaboost2:
         return weighted_sum
 
 class MultiClassClassifier:
-    def __init__(self, num_classes):
-        self.num_classes = num_classes
-        self.classifiers = [Adaboost2() for _ in range(num_classes)]
+    def __init__(self, T, A):
+        self.classifiers = [Adaboost2(T, A) for _ in range(10)]
 
     def fit(self, X, Y):
-        for i in range(self.num_classes):
+        for i in range(10):
             Y_binary = np.where(Y == i, 1, -1)
             self.classifiers[i].fit(X, Y_binary)
 
@@ -212,28 +202,25 @@ class MultiClassClassifier:
         predictions = np.argmax(scores, axis=0)
         return predictions
 
-def adaboost_test2(T, A):
+def adaboost2_test(T, A):
     # Load MNIST data
     X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
 
     # Create and train Adaboost classifier
-    clf = MultiClassClassifier(10)
+    clf = MultiClassClassifier(T, A)
 
     start_time = time.time()
     clf.fit(X_train, Y_train)
-    end_time = time.time()
 
     # Make predictions
     y_train_pred = clf.predict(X_train)
-    y_test_pred = clf.predict(X_test)
+    end_time = time.time()
 
     # Calculate accuracy scores
     train_accuracy = accuracy_score(Y_train, y_train_pred)
-    test_accuracy = accuracy_score(Y_test, y_test_pred)
 
     # Print accuracy scores
     print(f'Train accuracy: {train_accuracy * 100}%')
-    print(f'Test accuracy: {test_accuracy * 100}%')
     print(f'Time: {end_time - start_time} s')
 
     # Print additional information
@@ -252,20 +239,77 @@ def adaboost3_test():
 
     start_time = time.time()
     clf.fit(X_train, Y_train)
-    end_time = time.time()
 
     # Make predictions
     y_train_pred = clf.predict(X_train)
-    y_test_pred = clf.predict(X_test)
+    end_time = time.time()
 
     # Calculate accuracy scores
     train_accuracy = accuracy_score(Y_train, y_train_pred)
-    test_accuracy = accuracy_score(Y_test, y_test_pred)
 
     # Print accuracy scores
     print(f'Train accuracy: {train_accuracy * 100}%')
-    print(f'Test accuracy: {test_accuracy * 100}%')
     print(f'Time: {end_time - start_time} s')
+
+#Tarea 2B
+def adaboost3_grafic():
+    # Define the values of T and A to test
+    T_values = [10, 20, 30, 40, 50]
+    A_values = [10, 20, 30, 40, 50]
+
+    # Initialize empty lists for storing results
+    train_accuracies = []
+    train_times = []
+
+    # Load MNIST data
+    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
+
+    for T in T_values:
+        for A in A_values:
+            # Create and train Adaboost classifier
+            dtc = DecisionTreeClassifier(max_features=A)
+            clf = AdaBoostClassifier(estimator=dtc, n_estimators=T)
+            start_time = time.time()
+            clf.fit(X_train, Y_train)
+
+            # Make predictions
+            y_train_pred = clf.predict(X_train)
+            end_time = time.time()
+
+            # Calculate accuracy scores
+            train_accuracy = accuracy_score(Y_train, y_train_pred)
+
+            # Store results
+            train_accuracies.append(train_accuracy)
+            train_times.append(end_time - start_time)
+
+    # Reshape the results
+    train_accuracies = np.array(train_accuracies).reshape(len(T_values), len(A_values))
+    train_times = np.array(train_times).reshape(len(T_values), len(A_values))
+
+    # Create a figure and a subplot
+    fig, ax1 = plt.subplots()
+
+    # Plot accuracy
+    for i, A in enumerate(A_values):
+        ax1.plot(T_values, train_accuracies[:, i], label=f'Accuracy A={A}')
+    ax1.set_xlabel('T (number of iterations)')
+    ax1.set_ylabel('Accuracy')
+    ax1.legend(loc='upper left')
+
+    # Create a second y-axis for the same plot
+    ax2 = ax1.twinx()
+
+    # Plot training time
+    for i, A in enumerate(A_values):
+        ax2.bar(T_values, train_times[:, i], label=f'Training time A={A}', alpha=0.5)
+    ax2.set_ylabel('Training time (s)')
+    ax2.legend(loc='upper right')
+
+    fig.tight_layout()
+    ax1.set_xticks(T_values)
+    ax2.set_xticks(T_values)
+    plt.show()
 
 #Tarea 2D
 def keras_mlp_test():
@@ -298,5 +342,4 @@ def keras_mlp_test():
     print(f'Test accuracy: {test_acc * 100}%')
 
 if __name__ == '__main__':
-    #Tarea 1C
-    adaboost_grafic()
+    adaboost3_test()
